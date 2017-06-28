f22f56366b8c9a88ddf9a58f2666982696208323
Adrian Kuo <kuoadrian@gmail.com>
2017-06-28 12:39:10 -0700

    adjusting some design things, still shit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,9 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `
   <!-- header -->
-  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-drawer
-              mdl-layout--fixed-header">
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-drawer mdl-layout--fixed-header">
     <header class="mdl-layout__header mdl-color-text--white">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">Home</span>
         <div class="mdl-layout-spacer"></div>
       </div>
     </header>
@@ -24,36 +22,11 @@ import { Component } from '@angular/core';
 
     <main class="mdl-layout__content mdl-color--grey-100 mdl-color-text--grey-800">
       <div class="mdl-grid page-content">
-        <div class="mdl-cell--4-col">
-
-          <!-- Textfields and button -->
-          <form action="#">
-          <div class="mdl-textfield mdl-js-textfield">
-              <input class="mdl-textfield__input" type="text" placeholder="Item name...">
-          </div>
-
-          <div class="mdl-textfield mdl-js-textfield">
-              <input class="mdl-textfield__input" type="text" placeholder="Purchased for...">
-          </div>
-
-          <!-- Numeric Textfield -->
-          <div class="mdl-textfield mdl-js-textfield">
-              <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" placeholder="Price...">
-              <span class="mdl-textfield__error">Input is not a number!</span>
-          </div>
-
-          <button id="bt-update" type="reset" value="Reset" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">
-              Update
-          </button>
-          </form>
-
-        </div>
-
         <!-- Router Outlet -->
         <router-outlet></router-outlet>
       </div>
     </main>
-
+  </div>
   `,
 })
-export class AppComponent  {}
+export class AppComponent { }
--- src/app/groceries/grocery.component.ts
+++ src/app/groceries/grocery.component.ts
@@ -3,7 +3,28 @@ import { Component } from '@angular/core';
 @Component({
     template: `
     <h2>Grocery Splitter</h2>
-    <p>List of groceries</p>`
+    <div class="mdl-cell--4-col">
+        <!-- Textfields and button -->
+        <form action="#">
+        <div class="mdl-textfield mdl-js-textfield">
+            <input class="mdl-textfield__input" type="text" placeholder="Item name...">
+        </div>
+
+        <div class="mdl-textfield mdl-js-textfield">
+            <input class="mdl-textfield__input" type="text" placeholder="Purchased for...">
+        </div>
+
+        <!-- Numeric Textfield -->
+        <div class="mdl-textfield mdl-js-textfield">
+            <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" placeholder="Price...">
+            <span class="mdl-textfield__error">Input is not a number!</span>
+        </div>
+        
+        <button id="bt-update" type="reset" value="Reset" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">
+            Update
+        </button>
+        </form>
+    </div>`
 })
 
 export class GroceryComponent {}
