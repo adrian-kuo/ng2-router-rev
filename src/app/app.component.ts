9169d5739916eb193eb444a732cc295b474518f6
Adrian Kuo <kuoadrian@gmail.com>
2017-06-28 11:40:47 -0700

    basic structure working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,32 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <div class="demo-layout-transparent mdl-layout mdl-js-layout">
+      <header class="mdl-layout__header mdl-layout__header--transparent">
+        <div class="mdl-layout__header-row">
+
+          <!-- Title -->
+          <span class="mdl-layout-title">Scotch Pets</span>
+
+          <!-- Add spacer, to align navigation to the right -->
+          <div class="mdl-layout-spacer"></div>
+
+          <!-- Navigation with router directives-->
+          <nav class="mdl-navigation">
+            <a class="mdl-navigation__link" [routerLink]="['/']">Home</a>
+            <a class="mdl-navigation__link" [routerLink]="['/groceries']">Groceries</a>
+          </nav>
+        </div>
+      </header>
+
+      <main class="mdl-layout__content">
+        <h1 class="header-text">Categories</h1>
+      </main>
+    </div>
+
+    <!-- Router Outlet -->
+    <router-outlet></router-outlet>
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {}
