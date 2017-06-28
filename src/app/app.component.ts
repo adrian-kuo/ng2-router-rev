7c4bd2501bdc1aca7cf0a58729a4e27c2951f4ce
Adrian Kuo <kuoadrian@gmail.com>
2017-06-28 12:07:44 -0700

    Hideous but kinda there

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,31 +3,57 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-    <div class="demo-layout-transparent mdl-layout mdl-js-layout">
-      <header class="mdl-layout__header mdl-layout__header--transparent">
-        <div class="mdl-layout__header-row">
+  <!-- header -->
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-drawer
+              mdl-layout--fixed-header">
+    <header class="mdl-layout__header mdl-color-text--white">
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout-title">Home</span>
+        <div class="mdl-layout-spacer"></div>
+      </div>
+    </header>
 
-          <!-- Title -->
-          <span class="mdl-layout-title">Scotch Pets</span>
-
-          <!-- Add spacer, to align navigation to the right -->
-          <div class="mdl-layout-spacer"></div>
-
-          <!-- Navigation with router directives-->
-          <nav class="mdl-navigation">
-            <a class="mdl-navigation__link" [routerLink]="['/']">Home</a>
-            <a class="mdl-navigation__link" [routerLink]="['/groceries']">Groceries</a>
-          </nav>
-        </div>
-      </header>
-
-      <main class="mdl-layout__content">
-        <h1 class="header-text">Categories</h1>
-      </main>
+    <!-- drawer -->
+    <div class="mdl-layout__drawer mdl-color--white">
+      <span class="mdl-layout-title"><h4>Tools</h4></span>
+      <nav class="mdl-navigation">
+        <a class="mdl-navigation__link" [routerLink]="['/']">Home</a>
+        <a class="mdl-navigation__link" [routerLink]="['/groceries']">Groceries</a>
+      </nav>
     </div>
 
-    <!-- Router Outlet -->
-    <router-outlet></router-outlet>
+    <main class="mdl-layout__content mdl-color--grey-100 mdl-color-text--grey-800">
+      <div class="mdl-grid page-content">
+        <div class="mdl-cell--4-col">
+
+          <!-- Textfields and button -->
+          <form action="#">
+          <div class="mdl-textfield mdl-js-textfield">
+              <input class="mdl-textfield__input" type="text" placeholder="Item name...">
+          </div>
+
+          <div class="mdl-textfield mdl-js-textfield">
+              <input class="mdl-textfield__input" type="text" placeholder="Purchased for...">
+          </div>
+
+          <!-- Numeric Textfield -->
+          <div class="mdl-textfield mdl-js-textfield">
+              <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" placeholder="Price...">
+              <span class="mdl-textfield__error">Input is not a number!</span>
+          </div>
+
+          <button id="bt-update" type="reset" value="Reset" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">
+              Update
+          </button>
+          </form>
+
+        </div>
+
+        <!-- Router Outlet -->
+        <router-outlet></router-outlet>
+      </div>
+    </main>
+
   `,
 })
 export class AppComponent  {}
